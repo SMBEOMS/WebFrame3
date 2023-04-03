f584675e2afba53d9baa819ddfaf8aa84344771b
BEOMSEOK <jbs980612@gmail.com>
2023-04-03 15:27:39 +0900

    Post 모델 생성 및 시간 조정

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model): #일종의 DB구조, field설정
+    title = models.CharField(max_length=30)
+    contents = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)#자동으로 시간 맞춰줌
+    updated_at = models.DateTimeField(auto_now=True) #자동으로 시간 맞춰줌
+    # author
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
