319de5dcc61fc3baf217f9be93fe372ecf830f9e
BEOMSEOK <jbs980612@gmail.com>
2023-04-10 15:20:39 +0900

    웹페이지 완성

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('',views.index),
+    path('<int:pk>/', views.single_post_page), #bolg/숫자 넣어야 함
+]
+#pk = 숫자
